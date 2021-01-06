0138e3390d2f34822ea1e7eaab59abc9bc14f628
nigelfrnds <fernandes.nigelj@gmail.com>
2021-01-06 16:52:53 -0500

    webpack: add support for css

--- webpack/react-js/build-utils/webpack.common.js
+++ webpack/react-js/build-utils/webpack.common.js
@@ -1,5 +1,8 @@
-const webpack = require('webpack');
 const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.jsx'),
@@ -13,10 +16,33 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              url: true,
+              sourceMap: true
+            }
+          }
+        ]
       }
     ]
   },
   resolve: {
     extensions: ['*', '.js', '.jsx']
-  }
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css'
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '..', './public/index.html')
+    }),
+  ],
 };
--- webpack/react-js/build-utils/webpack.prod.js
+++ webpack/react-js/build-utils/webpack.prod.js
@@ -1,15 +1,4 @@
-const path = require('path');
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
 module.exports = {
   mode: 'production',
-  plugins: [
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './public/index.html')
-    })
-  ],
   devtool: 'source-map'
 };
